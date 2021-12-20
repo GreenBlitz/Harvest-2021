5a2c8e91cdce5d4c50f8644e3a37e41f5263f887
NovaCiduki <ciduki@gmail.com>
2021-12-20 14:36:16 +0200

    Ben - now we have HashMap<time,HashMap<button,isPressed>>

--- src/main/java/edu/greenblitz/bigRodika/Robot.java
+++ src/main/java/edu/greenblitz/bigRodika/Robot.java
@@ -5,7 +5,13 @@ import edu.greenblitz.bigRodika.subsystems.Chassis;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+import java.util.HashMap;
+
 public class Robot extends TimedRobot {
+
+    private double startTime;
+    private boolean recordDriver = true;//will be false when we need to stop the recording.
+    private HashMap<Double, HashMap<String, Double>> followDriverData;
 
     @Override
     public void robotInit() {
@@ -23,4 +29,38 @@ public class Robot extends TimedRobot {
     public void teleopInit() {
         new ArcadeDrive(OI.getInstance().getMainJoystick()).schedule();
     }
+
+    @Override
+    public void testInit() {
+        startTime = System.currentTimeMillis() / 1000.0;
+
+    }
+
+    @Override
+    public void testPeriodic() {
+        if (OI.getInstance().getMainJoystick().START.get() && recordDriver) {
+            recordDriver = false;
+
+        }
+        if (recordDriver) {
+            followDriverData.put((System.currentTimeMillis() / 1000.0) - startTime, OI.getInstance().getMainJoystick().getButtonsOn());
+        }
+    }
+    /* todo: finish function - make a csv string that represent the hashmap;
+    private <T, K> String HashMapToCsv(HashMap<T, K> myMap) {
+        String eol = System.getProperty("line.separator");
+
+        try (Writer writer = new FileWriter("somefile.csv")) {
+            for (Map.Entry<String, String> entry : myHashMap.entrySet()) {
+                writer.append(entry.getKey())
+                        .append(',')
+                        .append(entry.getValue())
+                        .append(eol);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace(System.err);
+        }
+    }*/
+
+
 }
